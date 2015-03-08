ec5950c8aeed565697839022ec01fddad294199a
e-sutton <eoin.sutton@live.com>
2015-03-08 11:43:16 +0000

    first commit

---
+++ src/omdb/OMDB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package omdb;
+
+/**
+ *
+ * @author Eoin
+ */
+public class OMDB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //set form to visible
+        GUI form = new GUI();
+        form.setVisible(true);
+    }
+    
+}
